c56b9b0b56cb51d4e190b300faa8e2c6ca9fe59e
ArtemAndZakhar <69362178+ArtemAndZakhar@users.noreply.github.com>
2021-04-16 00:11:55 +0300

    Пофиксили меню и шапку, добавили контент

---
+++ js/main.js
@@ -0,0 +1,18 @@
+    const swiper = new Swiper('.channel-slider', {
+        // Optional parameters
+        // direction: 'vertical',
+        slidesPerView: 6,
+        loop: true,
+    
+        // // If we need pagination
+        // pagination: {
+        // el: '.swiper-pagination',
+        // },
+    
+        // Navigation arrows
+        navigation: {
+        nextEl: '.channel-button-next',
+        prevEl: '.channel-button-prev',
+        },
+    });
+    
